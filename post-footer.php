553d354cd0ccbab35d22af36cd4d17165817b657
Ben Nuttall <ben@bennuttall.com>
2013-08-22 23:13:56 +0100

    Added Google Authorship

--- post-footer.php
+++ post-footer.php
@@ -1,3 +1,6 @@
 <footer>
-<?php if (!is_page()): show_tag_list(get_the_ID(), ', ', 'Tags: '); endif; ?>
+    <div class="author">by <a href="https://plus.google.com/109819740851612778825?rel=author" target="_blank">Ben Nuttall</a></div>
+    <?php if (!is_page()): ?>
+    <div class="tags"><?php show_tag_list(get_the_ID(), ', ', 'Tags: '); ?></div>
+    <?php endif; ?>
 </footer>
