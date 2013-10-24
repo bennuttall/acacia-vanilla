9509d938e519cfbc6a7d0ce5bcb52ab5a1e9909c
Ben Nuttall <ben@bennuttall.com>
2013-10-24 19:57:47 +0100

    Exclude current post from Featured Posts in sidebar

--- sidebar-featured-posts.php
+++ sidebar-featured-posts.php
@@ -6,6 +6,7 @@ $featured_args = array(
     'category_name' => 'featured',
     'orderby' => 'rand',
     'posts_per_page' => '5',
+    'post__not_in' => array(get_the_ID()),
 );
 
 $featured = new WP_Query($featured_args);
@@ -14,7 +15,7 @@ while ($featured->have_posts()): $featured->the_post();
 
 ?>
 
-<li>- <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+    <li>- <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
 
 <?php endwhile; ?>
 </ul>
