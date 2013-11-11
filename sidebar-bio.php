41548028f424066100c3cdf5d90c99a8e9b0610c
Ben Nuttall <ben@bennuttall.com>
2013-11-11 23:08:57 +0000

    Add gittip logo to sidebar

--- sidebar-bio.php
+++ sidebar-bio.php
@@ -1,7 +1,8 @@
 <h2><?php bloginfo('name'); ?></h2>
 <?php the_author_meta('description', 1); ?>
 <div id="icons">
-    <a href="https://plus.google.com/109819740851612778825" rel="publisher"><img src="/wp-content/themes/acacia-vanilla/images/gplus-32.png" alt="Google+" /></a>
-    <a href="https://www.github.com/bennuttall"><img src="/wp-content/themes/acacia-vanilla/images/GitHub-Mark-32px.png" alt="Github" /></a>
-    <a href="https://twitter.com/ben_nuttall"><img src="/wp-content/themes/acacia-vanilla/images/twitter-bird-white-on-blue-32x32.png" alt="Twitter" /></a>
+    <a href="https://plus.google.com/109819740851612778825" rel="publisher"><img src="<?php bloginfo('template_url'); ?>/images/gplus-32.png" alt="Google+" /></a>
+    <a href="https://www.github.com/bennuttall"><img src="<?php bloginfo('template_url'); ?>/images/GitHub-Mark-32px.png" alt="Github" /></a>
+    <a href="https://twitter.com/ben_nuttall"><img src="<?php bloginfo('template_url'); ?>/images/twitter-bird-white-on-blue-32x32.png" alt="Twitter" /></a>
+    <a href="https://www.gittip.com/ben_nuttall"><img src="<?php bloginfo('template_url'); ?>/images/gittip-32x32.png" alt="Gittip" /></a>
 </div>
