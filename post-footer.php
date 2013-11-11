7a2e5d4511855396a8a7c77b3fecbd878bd5f535
Ben Nuttall <ben@bennuttall.com>
2013-11-11 23:25:53 +0000

    Add gittip link to post footer

--- post-footer.php
+++ post-footer.php
@@ -2,5 +2,7 @@
 <footer>
     <div class="author">by <a href="<?php the_author_meta('user_url'); ?>" target="_blank" rel="author"><?php the_author(); ?></a></div>
     <div class="tags"><?php show_tag_list(get_the_ID(), ', ', 'Tags: '); ?></div>
+    <a href="https://www.gittip.com/ben_nuttall" target="_blank" class="gittip-support">Support me on gittip</a>
+    <a href="https://www.gittip.com/ben_nuttall" target="_blank" class="gittip-icon">&gt;3</a>
 </footer>
 <?php endif; ?>
