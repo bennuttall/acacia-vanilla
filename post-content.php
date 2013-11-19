71310b7cfd80f7c68a604daa8119b0b1bc4e53f1
Ben Nuttall <ben@bennuttall.com>
2013-11-19 10:28:12 +0000

    Cleanup

--- post-content.php
+++ post-content.php
@@ -1,14 +1,13 @@
 <?php if (get_post_type() == 'post' && !is_single()): ?>
-<div class="excerpt-holder">
+<div class="excerpt">
     <?php the_excerpt(); ?>
 </div>
 <aside class="post-thumb">
     <a href="<?php the_permalink(); ?>">
-    <?php the_post_thumbnail('thumbnail'); ?>
+        <?php the_post_thumbnail('thumbnail'); ?>
     </a>
 </aside>
-<?php
-    else:
+<?php else:
         the_content();
         get_template_part('post-footer');
-    endif;
+endif;
